121974e37dbdaa4e2ec16bb756bf527405ff490d
tobby <tobby@163.com>
2022-06-07 12:37:33 +0800

    2

---
+++ peripheral/pin_config.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-06-07     admin       the first version
+ */
+#ifndef PERIPHERAL_PIN_CONFIG_H_
+#define PERIPHERAL_PIN_CONFIG_H_
+//RADIO
+#define Radio_IRQ        38
+
+//BASIC
+#define Key0_PIN        22
+#define Key1_PIN         0
+#define LED1_PIN        19
+#define LED2_PIN        20
+#define LED3_PIN        44
+#define LED4_PIN        50
+#define BUZZER_PIN       1
+
+//WIFI
+#define WIFI_EN         21
+
+#endif /* PERIPHERAL_PIN_CONFIG_H_ */
